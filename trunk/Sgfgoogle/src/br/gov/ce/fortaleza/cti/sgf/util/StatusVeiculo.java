375a6cf4cf467417d15f95379ea8fd4575eeecd1
rlafitte.fa7@gmail.com <rlafitte.fa7@gmail.com@ca9364b1-c8e8-3f10-eab0-db27a1341943>
2010-12-02 12:55:22 +0000

    Lafitte - Status incluído TRANSF_EXTERNA

--- trunk/Sgfgoogle/src/br/gov/ce/fortaleza/cti/sgf/util/StatusVeiculo.java
+++ trunk/Sgfgoogle/src/br/gov/ce/fortaleza/cti/sgf/util/StatusVeiculo.java
@@ -2,5 +2,5 @@ package br.gov.ce.fortaleza.cti.sgf.util;
 
 public class StatusVeiculo {
 
-	public static Integer DISPONIVEL = 0, LOCADO = 1, RESERVADO = 2, EM_MANUTENCAO = 3, EM_SERVICO = 4, DELETADO = -1;
+	public static Integer DISPONIVEL = 0, LOCADO = 1, RESERVADO = 2, EM_MANUTENCAO = 3, EM_SERVICO = 4, DELETADO = -1, TRANSF_EXTERNA = 5;
 }
